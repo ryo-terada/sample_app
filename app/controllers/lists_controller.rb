e90bbe8ae99c4c3a51ae54e17e6d214be0a91a7d
ryo-terada <ryo214099@gmail.com>
2022-05-20 19:34:00 +0000

    [Finish]3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,12 +4,12 @@ class ListsController < ApplicationController
   end
   
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    # 4. 詳細画面へリダイレクト
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if@list.save
+      redirect_to list_path(list.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -21,8 +21,22 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
   end
   
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+  
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
+  
+
   private
   # ストロングパラメータ
   def list_params
